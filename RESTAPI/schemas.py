8594b944221807b1abcd13676f1af92bb50450ef
John <johnnjau202@gmail.com>
2024-08-27 13:13:26 +0300

    booking.db

--- RESTAPI/schemas.py
+++
@@ -1,8 +0,0 @@
-from flask_marshmallow import Marshmallow
-from models import VehicleBooking  # Adjust the import path if necessary
-
-ma = Marshmallow()
-
-class VehicleBookingSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = VehicleBooking
