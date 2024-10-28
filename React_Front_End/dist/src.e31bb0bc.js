da26288c41fc8bab463f67be63aa2ba91972b3ca
John <johnnjau202@gmail.com>
2024-10-28 15:47:17 +0300

    test

--- React_Front_End/dist/src.e31bb0bc.js
+++ React_Front_End/dist/src.e31bb0bc.js
@@ -25698,7 +25698,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36645" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34803" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
